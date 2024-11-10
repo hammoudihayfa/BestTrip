72e4d58b2f4c0a81bbdaa1e75bfdaac5c3837a21
hayfa <125963157+hammoudihayfa@users.noreply.github.com>
2024-11-10 16:26:59 +0100

    Delete MSRéservationVoyage directory

--- MSRéservationVoyage/BestTripBack/src/main/java/tn/esprit/besttripback/Repository/TripRepository.java
+++
@@ -1,7 +0,0 @@
-package tn.esprit.besttripback.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tn.esprit.besttripback.Entity.Trip;
-
-public interface TripRepository extends JpaRepository<Trip,Long> {
-}
