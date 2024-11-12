21a3f46dbaf54c7b327ec1ab429202cbb78bf514
Hammoudi Hayfa <hammoudi.hayfa@esprit.tn>
2024-11-12 10:31:17 +0100

    projet

---
+++ ConfigServerProj/src/test/java/tn/esprit/configserverproj/ConfigServerProjApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.configserverproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
